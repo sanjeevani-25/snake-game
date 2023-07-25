3270872b17903c84afa404cd50b3be45521d3897
sanjeevani-25 <sanjeevani.lakade25@gmail.com>
2023-07-25 19:11:51 +0530

    nm

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const playBoard = document.querySelector("play-board");
+let foodX = 13;
+let foodY = 10;
+
+const initGame = () => {
+  let htmlMarkup = `<div class"food" style="grid-area: ${foodY}/${foodX}></div>`;
+};
 
